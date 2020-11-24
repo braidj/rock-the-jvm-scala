6248d96f4acdd2465132bef719d1398cce40c463
Jason Braid <braidj@gmail.com>
2020-11-24 14:51:13 +0000

    Added notes on sugar and recorded exercises

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -1,11 +1,20 @@
 package lectures.part2oop
 
+import scala.language.postfixOps
+
 object MethodNotations extends App {
 
   class Person(val name:String, favouriteFilm: String) {
-    def likesMovie(movie: String): Boolean = movie == favouriteFilm
 
-    def +(person: Person): String = s"${this.name} hangs out with ${person.name}"
+    def likesMovie(movie: String): Boolean = movie == favouriteFilm  // infix notation
+
+    def +(person: Person): String = s"${this.name} hangs out with ${person.name}" // method operator
+
+    def unary_! : String = "What the heck"// need a space before : other wise compiler will think it is part of the method name
+
+    def isAlive: Boolean = true // methods with no parameters can be used for PostFixNotation
+
+    def apply(): String = s" Hi, my name is $name and I like $favouriteFilm" // Apply, methd name VERY important
 
   }
 
@@ -18,6 +27,27 @@ object MethodNotations extends App {
   //println(mary hangsOutWith mark) // methods can be called another, so if rename +
   println(mary + mark) // Operators in Scala are just methods
 
+  println(!mary) // Prefix notation, but only works with 4 operators: = - ! and ~
+  println(mary.unary_!) // equivalent
+
+  // Post Fix notation, but is rarely used in practice
+  println(mary isAlive)
+
+  // Apply
+  println(mary.apply())
+  println(mary()) // equivalent syntactic sugar. treats mary like a function, calling the special method of Apply
+
+
   //Syntactic sugar exercises
+  // 1. Overload the + operator, MAry + "the rockstar" => new person "MAry (the rockstar)"
+
+  // 2. Add age to Person class. Add nary + operator => new person with age + 1
+  // +mary => mary with age incremented
+
+  // 3. Add a "learns" method in person class => MAry learns 'Scala'
+  // then add a learnsScala method, calls above with Scala. Use it in postfix notation
+
+  // 4. Overload the apply method
+  // receives a nos, output => "Mary watched <movie> <2> times"
 
 }
--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -2,9 +2,103 @@ package lectures.part2oop
 
 object OOBasics extends App {
 
-  val person = new Person
-  println(person)
+  val person = new Person("Jon",26)
+  println(person.age)
+  println(person.x)
+  println(person.greet("Daniel"))
+  println(person.greet())
+
+  val JB = new Writer("Jason", "Braid", 1969)
+  println(JB.fullname)
+
+  val firstBook = new Novel("Casually deliberate",1987,JB)
+
+  println(firstBook.authorAge)
+  println(firstBook.isWrittenBy)
+  val newEdition = firstBook.copy(1998)
+  println(newEdition.authorAge)
+
+  val cnt = new Counter(7)
+
+  cnt.increase.print // 7 + 1 = 8
+  cnt.increase(5).print // 7 + 40 = 47
+
+  cnt.decrease.print // 7 -1 = 6
+  cnt.decrease(20).print // 7 - 20 = -13
+
 
 }
 
-class Person
+// constructor
+class Person(name: String, val age: Int) {
+  // Classes are blueprints that describe the way things looks like -> data,
+  // and the way that things work -> functionality or Behaviour
+  //
+  // class parameters are NOT FIELDS
+  // To convert just add the key ord of val or var before the parameter
+
+  // body, n.b. all expressions are evulated when a class is instantiated
+  val x = 2 // these are fields
+
+  println(1 + 3)
+
+  // a def inside a class is called a Method, note use of this key word
+  def greet(name: String): Unit = println(s"${this.name} says: Hi $name")
+
+  //overloading
+  def greet(): Unit = println(s"Hi I am $name")
+
+  // multiple constructors
+  // You can have auxilary constructors, but alot of the time can be replaced with defaults
+  def this(name:String) = this(name,0)
+  def this() = this("John Doe")
+
+}
+
+  /*
+    Exercises
+    Novel and a Writer classes
+
+   */
+
+class Writer(fName: String, lName: String, val age: Int) {
+    def fullname: String = s"$fName $lName"
+  }
+
+class Novel(name:String, yor: Int, author: Writer) {
+
+  def authorAge: Int = yor - author.age
+
+  def isWrittenBy: String = this.author.fullname
+
+  def copy(nyor: Int) = new Novel(name, nyor, author)
+
+}
+
+//Counter class
+class Counter(val value: Int) {
+
+  def increase = {
+    println("incrementing")
+    new Counter(value + 1)
+  } // immutability
+
+  def increase(amt:Int): Counter = {
+    if (amt <= 0) this
+    else increase.increase(amt-1)
+  }
+
+  def decrease  = {
+    println("Decrementing")
+    new Counter(value - 1)
+  }
+
+  def decrease(amt:Int): Counter = {
+    if (amt <= 0) this
+    else decrease.decrease(amt - 1)
+  }
+
+  def print: Unit = println(value)
+}
+
+
